b303c109e43054a41cc65acd56cbd01c3d27ad74
Gonçalo <Goncalofds@gmail.com>
2019-05-10 23:02:59 +0100

    make_instance, get_slot and set_slot!

--- jos.jl
+++ jos.jl
@@ -15,3 +15,37 @@ end
 @defclass(C1, [], a)
 @defclass(C2, [], b, c)
 @defclass(C3, [C1, C2], d)
+
+println(C1)
+
+mutable struct StandardInstance
+	class::StandardClass
+	slots::Dict
+end
+
+function make_instance(class::StandardClass, args...) 
+	instance = StandardInstance(class, Dict(args))
+	return instance
+end
+
+c3i1 = make_instance(C3, :a=>1, :b=>2, :c=>3, :d=>4)
+c3i2 = make_instance(C3, :b=>2)
+
+println(c3i1)
+println(c3i2)
+
+function get_slot(obj::StandardInstance, slot)
+	return obj.slots[slot]
+end
+
+println(get_slot(c3i2, :b))
+
+function set_slot!(obj::StandardInstance, slot, value)
+	obj.slots[slot] = value
+end
+
+set_slot!(c3i2, :b, 3)
+
+println(get_slot(c3i2, :b)) 
+println([get_slot(c3i1, s) for s in [:a, :b, :c]])
+
